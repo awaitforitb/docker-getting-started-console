06117a2f8b32f6a853b1bf4b823ba21f9b5c7958
Abhishek Ghag <abhisheksghag@gmail.com>
2020-01-26 18:10:52 -0500

    refactor(new-line): place braces on same line

--- DockerCounterConsole/Program.cs
+++ DockerCounterConsole/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace DockerCounterConsole
-{
-    class Program
-    {
-        private static void Main(string[] args)
-        {
+namespace DockerCounterConsole {
+    class Program {
+        private static void Main(string[] args) {
             Console.WriteLine("Start App.");
 
             var counter = 0;
             var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-            while (max == -1 || counter < max)
-            {
+            while (max == -1 || counter < max) {
                 counter++;
                 Console.WriteLine($"Counter: {counter}");
                 Task.Delay(5000).Wait();
